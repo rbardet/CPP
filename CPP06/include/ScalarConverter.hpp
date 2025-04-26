bdbab51c019c858cb13a5a1a2ba54841e9b8ca17
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-04-26 06:58:39 +0200

    rep ElPueblo

---
+++ CPP06/include/ScalarConverter.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbardet- <rbardet-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 06:57:36 by rbardet-          #+#    #+#             */
+/*   Updated: 2025/04/26 06:57:49 by rbardet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class ScalarConverter
+{
+private:
+	/* data */
+public:
+	ScalarConverter(/* args */);
+	~ScalarConverter();
+};
+
+
