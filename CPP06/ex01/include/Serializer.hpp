76cbf0ac3ba470f495d0cbb8fb8ae56b24b11a8e
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-04-28 06:57:32 +0200

    CPP06->ex01 done

--- CPP06/ex01/include/Serializer.hpp
+++ CPP06/ex01/include/Serializer.hpp
@@ -6,21 +6,31 @@
 /*   By: rbardet- <rbardet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 06:20:23 by rbardet-          #+#    #+#             */
-/*   Updated: 2025/04/28 06:25:05 by rbardet-         ###   ########.fr       */
+/*   Updated: 2025/04/28 06:50:32 by rbardet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <stdint.h>
+
+typedef struct s_data
+{
+	char	super;
+	int		cool;
+}				Data;
 
 class Serializer
 {
 private:
 	Serializer();
+	Serializer(const Serializer &src);
 public:
+	Serializer	operator=(const Serializer &src);
 	~Serializer();
 
 	static uintptr_t	serialize(Data *ptr);
 	static Data			*deserialize(uintptr_t raw);
 };
+
--- CPP06/ex01/src/Serializer.cpp
+++ CPP06/ex01/src/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: rbardet- <rbardet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 06:20:20 by rbardet-          #+#    #+#             */
-/*   Updated: 2025/04/28 06:25:50 by rbardet-         ###   ########.fr       */
+/*   Updated: 2025/04/28 06:49:19 by rbardet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@ Serializer::Serializer()
 
 }
 
+Serializer::Serializer(const Serializer &src)
+{
+	*this = src;
+}
+
+Serializer	Serializer::operator=(const Serializer &src)
+{
+	return (src);
+}
+
 Serializer::~Serializer()
 {
 
@@ -24,11 +34,11 @@ Serializer::~Serializer()
 
 uintptr_t	Serializer::serialize(Data *ptr)
 {
-
+	return (reinterpret_cast<uintptr_t>(ptr));
 }
 
 Data	*Serializer::deserialize(uintptr_t raw)
 {
-
+	return (reinterpret_cast<Data *>(raw));
 }
 
