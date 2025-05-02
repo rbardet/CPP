8c04280c37420d1f720bdbed329719f5c9bf3255
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-05-02 14:14:17 +0200

    FF

---
+++ CPP09/ex00/include/BitcoinExchange.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbardet- <rbardet-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 14:11:55 by rbardet-          #+#    #+#             */
+/*   Updated: 2025/05/02 14:12:49 by rbardet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+class BitcoinExchange
+{
+private:
+	/* data */
+public:
+	BitcoinExchange(/* args */);
+	~BitcoinExchange();
+};
+
+
+
---
+++ CPP09/ex00/src/BitcoinExchange.cpp
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbardet- <rbardet-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 14:12:03 by rbardet-          #+#    #+#             */
+/*   Updated: 2025/05/02 14:12:30 by rbardet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {}
+
+BitcoinExchange::~BitcoinExchange() {}
