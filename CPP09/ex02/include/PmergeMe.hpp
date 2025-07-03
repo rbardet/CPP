d2cfd88596ebcb5156d815ada3e0b4a5c435b43c
Robin <robinbt2004@gmail.com>
2025-07-03 11:10:22 +0200

    Create ex02

---
+++ CPP09/ex02/include/PmergeMe.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robin <robin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 10:56:18 by robin             #+#    #+#             */
+/*   Updated: 2025/07/03 10:59:29 by robin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "algorithm"
+#include "iostream"
+
+#define WRONGARGNB "Error: you need to pass at least one arg"
+#define ONLYPOSITIVE "Error: only postive number allowed"
